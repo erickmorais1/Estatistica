27d74ea7b6f762562d5660918f15db18b93d525b
erick morais <erick.carvalho@alunos.ufersa.edu.br>
2026-06-01 17:24:55 -0300

    Método da mediana

--- src/br/edu/ufersa/Estatistica/EstatisticaDescritiva.java
+++ src/br/edu/ufersa/Estatistica/EstatisticaDescritiva.java
@@ -1,5 +1,6 @@
 package br.edu.ufersa.Estatistica;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class EstatisticaDescritiva {
     public double media(ArrayList<Double> dados) {
@@ -15,11 +16,33 @@ public class EstatisticaDescritiva {
                 //retorna a media(soma dos termos / quantidade de termos)
                 return soma / contador;
             } else {
-                //-999 é o valor padrão para indicar erro
+                //-999 é o noso valor padrão para indicar erro
                 return -999;
             }
         }else{
             return -999;
         }
-    };
+    }
+    public double mediana(ArrayList<Double> dados) {
+        if (dados != null) {
+            //ordena a lista de forma crescente
+            Collections.sort(dados);
+
+            //caso a quantidade de dados seja par
+            if (dados.size() % 2 == 0) {
+                int indice1 = (dados.size() / 2) - 1; //pega o valor que está no ìncide
+                int indice2 = (dados.size() / 2);//pega o valor que está no indice
+
+                //os números nos índices são somados e divididos por 2
+                double mediana = (dados.get(indice1) + dados.get(indice2)) / 2;
+                return mediana;
+            } else {
+                //caso a quantidade de dados seja ímpar
+                //o valor que está no índice *dados.size()/2* será a mediana
+                double mediana = dados.get(dados.size() / 2);
+                return mediana;
+            }
+        }else return -999;
+    }
+
 }
